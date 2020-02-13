d29a7563692725faf6a446a289419b0973f4de62
msureshk <msureshk@iu.edu>
2020-02-13 01:20:42 +0000

    add semaphores

--- apps/consume.c
+++ apps/consume.c
@@ -1,5 +1,6 @@
 #include <xinu.h>
 #include <prodcons.h>
+#include <prodcons_bb.h>  
 
 void consumer(int count) {
 	for (int i =1; i<=count; i++){
@@ -7,3 +8,16 @@ void consumer(int count) {
 
 	}
 }
+
+void consumer_bb(int count) { 
+	  char *proc_name = proctab[getpid()].prname;
+	  for(int i = 0; i<count; i++){
+		wait(csem);
+		wait(mutex);
+		kprintf("name : %s, read : %d\n",proc_name,arr_q[readq]);
+		readq = (readq+1)%BUFFSIZE;
+		signal(mutex);
+		signal(psem);
+	}	
+}
+
--- apps/produce.c
+++ apps/produce.c
@@ -1,9 +1,27 @@
 #include <xinu.h>
 #include <prodcons.h>
+#include <prodcons_bb.h>  
+
+//int arr_q, write, read, BUFFSIZE;
+//sid32 psem,csem,mutex;
 
 void producer(int count) {
 	for (int i =1; i<=count; i++){
 		n=i;
 		printf("produced : %d\n",n);
 	}		
+}
+
+void producer_bb(int count) {
+	  // Iterate from 0 to count and for each iteration add iteration value to the global array `arr_q`, 
+        char *proc_name = proctab[getpid()].prname;  
+	for(int i = 1; i<=count; i++){
+	   	wait(psem);
+	   	wait(mutex);
+	  	arr_q[writeq]=i;
+	  	kprintf("name : %s, write : %d\n",proc_name,i);
+	  	writeq = (writeq+1)%BUFFSIZE;
+	  	signal(mutex);
+	  	signal(csem);
+	}
 }
