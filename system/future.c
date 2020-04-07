830a86bb67c056dcc7e938092cb39c7d7581560d
Suresh Kumar, Manisha <msureshk@iu.edu>
2020-04-07 11:53:35 -0400

    add FUTURE_QUEUE

--- system/future.c
+++ system/future.c
@@ -5,13 +5,18 @@ future_t* future_alloc(future_mode_t mode, uint size, uint nelems){
 	intmask mask;
 	mask = disable();
 	future_t* new_future = (future_t*)getmem(sizeof(future_t));
-	new_future->data = (char *)getmem(sizeof(char) * size);
+	new_future->data = (char *)getmem(sizeof(char) * size * nelems);
 	new_future->state = FUTURE_EMPTY;
 	new_future->mode = mode;
 	new_future->size = size;
 	new_future->pid= 0;
 	new_future->set_queue = NULL;
 	new_future->get_queue = NULL;
+	
+	new_future->max_elems = nelems;
+	new_future->uint16 count = 0;
+	new_future->uint16 head = 0;
+	new_future->uint16 tail = 0;
 	restore(mask);
 	return new_future;
 }
@@ -103,6 +108,40 @@ syscall future_get(future_t* f, char* data){
 		//kprintf("in get and about to memcpy %d\n",(int)f->data);
 		memcpy(data,f->data,f->size);
 	}
+	else if(f->mode == FUTURE_QUEUE){
+		if(f->state == FUTURE_EMPTY){
+			pid32 pid = getpid();
+			f->state = FUTURE_WAITING;
+			if (f->get_queue == NULL){ //First process to request for access to future
+				f->get_queue = (queue*)getmem(sizeof(queue));
+				f->get_queue->pid = pid;
+				f->get_queue->next = NULL;
+			}
+			else{
+				queue* temp = f->get_queue;
+				while(temp->next != NULL)
+					temp = temp->next;
+				temp->next = (queue*)getmem(sizeof(queue));
+				temp->next->pid = pid;
+				temp->next->next = NULL;
+			}
+			suspend(pid);
+		}
+		if(f->count != 0){
+			char* headelemptr = f->data + (f->head * f->size);
+			memcpy(data,headelemptr,f->size);
+			f->head += 1;
+			f->count -= 1;
+			if (f->count == 0)
+				f->state = FUTURE_EMPTY;
+		}
+		
+		if(!f->set_queue){
+			struct queue* tmp = f->set_queue;
+			f->set_queue = f->set_queue->next;
+			resume(tmp->pid);	
+		}
+	}
 	restore(mask);	
 	return OK;
 }
@@ -152,6 +191,39 @@ syscall future_set(future_t* f, char* data){
 			restore(mask);
 			return SYSERR;//Future data can be set only once
 		}
+	}
+	else if(f->mode == FUTURE_QUEUE){
+		if(f->count == f->max_elems){
+				pid32 pid = getpid();
+				if (f->set_queue == NULL){ //First process to request for access to future
+					f->set_queue = (queue*)getmem(sizeof(queue));
+					f->set_queue->pid = pid;
+					f->set_queue->next = NULL;
+				}
+				else{
+					queue* temp = f->set_queue;
+					while(temp->next != NULL)
+						temp = temp->next;
+					temp->next = (queue*)getmem(sizeof(queue));
+					temp->next->pid = pid;
+					temp->next->next = NULL;
+				}
+				suspend(pid);
+		}
+		if(f->count != f->max_elems){
+			char* tailelemptr = f->data + (f->tail * f->size);
+			memcpy(tailelemptr,data,f->size);
+			f->tail += 1;
+			f->count += 1;
+			f->state = FUTURE_READY;
+		}	
+		
+		if(!f->get_queue){
+			struct queue* tmp = f->get_queue;
+			f->get_queue = f->get_queue->next;
+			resume(tmp->pid);			
+		}
+			
 	}
 	restore(mask);
 	return OK;
