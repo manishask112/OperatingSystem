ba927af966290c092ab2b6939449a076dc663bbd
msureshk <msureshk@iu.edu>
2020-04-08 15:37:14 -0400

    add tscdf_fq to xsh_run

--- shell/xsh_run.c
+++ shell/xsh_run.c
@@ -200,6 +200,8 @@ shellcmd xsh_run(int nargs, char *args[]){
 			resume (create((void *)futures_test, 4096, 20, "futures_test", 1, args));
 			
 	}
+	else if (strncmp(args[1],"tscdf_fq",8) == 0)
+		resume (create((int*)stream_proc_futures, 4096, 20, "stream_proc_futures", 2, nargs, args));
 	else if(strncmp(args[1],"tscdf",5) == 0)
 		resume (create((int*)stream_proc, 4096, 20, "stream_proc", 2, nargs, args));
 }	
