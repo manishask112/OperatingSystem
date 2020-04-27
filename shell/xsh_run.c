c1fbaab6606266fa86f17fa385dd51dc05c01c7f
msureshk <msureshk@iu.edu>
2020-04-27 13:39:32 -0400

    a7

--- shell/xsh_run.c
+++ shell/xsh_run.c
@@ -204,4 +204,7 @@ shellcmd xsh_run(int nargs, char *args[]){
 		resume (create((int*)stream_proc_futures, 4096, 20, "stream_proc_futures", 2, nargs, args));
 	else if(strncmp(args[1],"tscdf",5) == 0)
 		resume (create((int*)stream_proc, 4096, 20, "stream_proc", 2, nargs, args));
+	else if(strncmp(args[1],"fstest",6) == 0)
+		resume(create((uint *) fstest, 4096, 20, "fstest", 2,nargs, args));
+
 }	
